6b9cf5fb41f6e83e3539975228bddd242bf1c4dd
senerudev <sarath@seneru.com>
2014-09-28 07:39:54 +0530

    updated

--- login.php
+++ login.php
@@ -268,7 +268,7 @@ $(document).ready(function() {
                 },				
                 password: {
                     required: true,
-					minlength: 6
+					minlength: 3
                 },
 				
             },
