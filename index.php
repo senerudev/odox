b33a222b64018d7335afe4121877f11e1a317f99
senerudev <sarath@seneru.com>
2014-09-28 07:28:49 +0530

    updated

--- index.php
+++ index.php
@@ -1228,11 +1228,8 @@ jQuery(document).ready(function($)
 		function initialize(){
 			
  var locations = [
-          ['Bondi Beach', -33.890542, 151.274856, 4],
-          ['Coogee Beach', -33.923036, 151.259052, 5],
-          ['Cronulla Beach', -34.028249, 151.157507, 3],
-          ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-          ['Maroubra Beach', -33.950198, 151.259302, 1]
+          ['Colombo Sri Lanka', 6.9215305,79.8648533, 2],
+          ['Sweden', 59.8332794,17.6584471, 1]
       ];
       
       <?php 
