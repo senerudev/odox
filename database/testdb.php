13783bb976b3b7e2462a7b3071ce0cf4ff9ffc33
senerudev <sarath@seneru.com>
2014-09-28 12:25:44 +0530

    testing

---
+++ database/testdb.php
@@ -0,0 +1,16 @@
+<?php
+$con=mysqli_connect("54.169.80.82","root","odox","odox");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM user");
+
+while($row = mysqli_fetch_array($result)) {
+  echo $row['userName'] . " " . $row['name'];
+  echo "<br>";
+}
+
+mysqli_close($con);
+?>
