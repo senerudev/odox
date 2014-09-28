d95efadb86b3ecf4856049282eebcb57c5035667
senerudev <sarath@seneru.com>
2014-09-28 08:25:34 +0530

    updated

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 class Configuration
 {
 	// Database settings
-	const HOSTNAME = "54.169.80.82:3306";
+	const HOSTNAME = "54.169.80.82";
 	const DATABASENAME = "odox";
 	const USERNAME = "root";
 	const PASSWORD = "gfzeoqfo";
